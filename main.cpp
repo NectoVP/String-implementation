bbd77045018ad48e182a568072eca4e0c8fa1c32
pt_va <pt_va@bk.ru>
2024-08-19 13:42:37 +0300

    added some basic methods

--- main.cpp
+++ main.cpp
@@ -14,6 +14,10 @@ private:
 		std::swap(arr, other.arr);
 	}
 
+	void check(size_t idx) const {
+		if(idx >= sz)
+			throw std::runtime_error("index is out of range");
+	}
 public:
 	String() {}
 
@@ -40,6 +44,62 @@ public:
 
 	friend std::ostream& operator<<(std::ostream&, const String& str);
 
+	const char& operator[](size_t idx) const {
+		check(idx);
+		return arr[idx];
+	}
+
+	char& operator[](size_t idx) {
+		check(idx);
+		return arr[idx];
+	}
+
+	size_t length() const {
+		return sz;
+	}
+
+	size_t size() const {
+		return sz;
+	}
+
+	size_t capacity() const {
+		return cap - 1;
+	}
+
+	void pop_back() {
+		--sz;
+		arr[sz] = '\0';
+	}
+
+	void push_back(char c) {
+		if(sz == cap - 1) {
+			cap *= 2;
+			char* new_arr = new char[cap];
+			std::copy(arr, arr + sz, new_arr);
+			delete[] arr;
+			arr = new_arr;
+		}
+
+		arr[sz] = c;
+		arr[++sz] = '\0';
+	}
+
+	const char& front() const {
+		return arr[0];
+	}
+
+	char& front() {
+		return arr[0];
+	}
+
+	const char& back() const {
+		return arr[sz - 1];
+	}
+
+	char& back() {
+		return arr[sz - 1];
+	}
+
 private:
 	size_t sz = 0;
 	size_t cap = 0;
@@ -52,4 +112,5 @@ std::ostream& operator<<(std::ostream& os, const String& str) {
 }
 
 int main() {
+
 }
